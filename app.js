7afd4df1e7cbf45c71c3212c6caae9bc49de73e1
mdevk093 <mandev101@gmail.com>
2020-11-28 19:45:46 -0500

    Score animation and more animation

--- app.js
+++ app.js
@@ -34,28 +34,31 @@ var factoryPrice = 12000;
 var bank = 0;
 var bankPrice = 69000;
 
+var speed = 100;
+var interval = 1;
+
 
 setInterval(() => {
     if((monkey > 0) || (mine > 0) || (farm > 0) || (factory > 0) || (bank > 0)){
         if(monkey > 0){
             score += (monkey*2);
-            displayScore.textContent = score;
+            //displayScore.textContent = score;
         } 
         if(mine > 0){
             score += (mine*8);
-            displayScore.textContent = score;
+            //displayScore.textContent = score;
         }
         if(farm > 0){
             score += (farm*20);
-            displayScore.textContent = score;
+            //displayScore.textContent = score;
         }
         if(factory > 0){
             score += (factory*46);
-            displayScore.textContent = score;
+            //displayScore.textContent = score;
         }
         if(bank > 0){
             score += (bank*46);
-            displayScore.textContent = score;
+            //displayScore.textContent = score;
         }
     }
     else{
@@ -63,9 +66,31 @@ setInterval(() => {
     }
 }, 1000);
 
+if(persecond > 1){
+    speed = 500;
+}
+if(persecond > 10){
+    speed = 100;
+}
+if(persecond > 100 ){
+    interval = 10;
+}
+
+let output = 0;
+setInterval(() =>{
+    if(output < score){
+        displayScore.textContent = output;
+        output+= interval;
+    } else {
+        clearInterval();
+    }
+},speed);
+
+
+
 click.addEventListener('click', () =>{
     score += 1;
-    displayScore.textContent = score;
+    //displayScore.textContent = score;
     click.style.transition = "0.3s"
     click.style.transform = "translate(-50%, -60%)";
     setTimeout(() =>{
@@ -85,6 +110,8 @@ clickMonkey.addEventListener('click', () =>{
     if(score >= monkeyPrice){
         monkey += 1;
         score -= monkeyPrice;
+        output -= monkeyPrice;
+        displayScore.textContent = score;
         monkeyPrice = Math.ceil(monkeyPrice * 1.2);
         persecond += 2;
         monkeyTotal.textContent = `x${monkey}`;
@@ -105,6 +132,8 @@ clickMine.addEventListener('click', () =>{
     if(score >= minePrice){
         mine += 1;
         score -= minePrice;
+        output -= minePrice;
+        displayScore.textContent = score;
         minePrice = Math.ceil(minePrice * 1.2);
         persecond += 8;
         mineTotal.textContent = `x${mine}`;
@@ -126,6 +155,8 @@ clickFarm.addEventListener('click', () =>{
     if(score >= farmPrice){
         farm += 1;
         score -= farmPrice;
+        output -= farmPrice;
+        displayScore.textContent = score;
         farmPrice = Math.ceil(farmPrice * 1.2);
         persecond += 20;
         psecond.textContent = persecond;
@@ -146,6 +177,8 @@ clickFactory.addEventListener('click', () =>{
     if(score >= factoryPrice){
         factory += 1;
         score -= factoryPrice;
+        output -= factoryPrice;
+        displayScore.textContent = score;
         factoryPrice = Math.ceil(factoryPrice * 1.2);
         persecond += 46;
         factoryTotal.textContent = `x${factory}`;
@@ -166,6 +199,8 @@ clickBank.addEventListener('click', () =>{
     if(score >= bankPrice){
         bank += 1;
         score -= bankPrice;
+        output -= bankPrice;
+        displayScore.textContent = score;
         bankPrice = Math.ceil(bankPrice * 1.2);
         persecond += 112;
         psecond.textContent = persecond;
@@ -192,3 +227,5 @@ function bought(){
         show.style.display = "none";
     },1000)
 }
+
+
